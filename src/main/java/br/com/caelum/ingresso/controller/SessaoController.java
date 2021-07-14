99de44dc5a1a95d6f56b01938c3ef7a04600ef9f
rodrigo penna <pennarodrigo@hotmail.com>
2021-07-14 20:11:40 -0300

    corrigindo a classe controller

--- src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
+++ src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
@@ -3,6 +3,7 @@ package br.com.caelum.ingresso.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -12,6 +13,7 @@ import br.com.caelum.ingresso.dao.SalaDao;
 import br.com.caelum.ingresso.model.Filme;
 import br.com.caelum.ingresso.model.Sala;
 
+@Controller
 public class SessaoController {
 	
 	@Autowired
