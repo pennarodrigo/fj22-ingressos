be16fe10fe636898e4f2c4cedcde1095016abd9b
Rodrigo Penna <pennarodrigo@hotmail.com>
2021-07-19 11:35:25 -0300

    incluir testes unitarios de sessao

---
+++ src/test/java/br/com/caelum/ingresso/validacao/GerenciadorDeSessaoTest.java
@@ -0,0 +1,23 @@
+package br.com.caelum.ingresso.validacao;
+
+import java.time.Duration;
+import java.time.LocalTime;
+
+import org.aspectj.lang.annotation.Before;
+import org.junit.Test;
+
+import br.com.caelum.ingresso.model.Filme;
+import br.com.caelum.ingresso.model.Sala;
+import br.com.caelum.ingresso.model.Sessao;
+
+public class GerenciadorDeSessaoTest {
+
+	private Filme rogueOne;
+	private Sala sala3D;
+	private Sessao sessaoDasDez;
+	private Sessao sessaoDasTreze;
+	private Sessao sessaoDasDezoito;
+
+
+
+}
