6d0036807cc752d29a5e3c54b7fb6d0f5e9b9b08
Rodrigo Penna <pennarodrigo@hotmail.com>
2021-07-19 22:15:47 -0300

    acertando o teste unitario de desconto via interface

--- src/test/java/br/com/caelum/ingresso/model/descontos/DescontoTest.java
+++ src/test/java/br/com/caelum/ingresso/model/descontos/DescontoTest.java
@@ -17,7 +17,7 @@ public class DescontoTest {
 	public	void	naoDeveConcederDescontoParaIngressoNormal() {
 					Sala	sala	=	new	Sala("Eldorado	-	IMAX",	new	BigDecimal("20.5"));
 					Filme	filme	=	new	Filme("Rogue	One",	Duration.ofMinutes(120),
-																					"SCI-FI",	new	BigDecimal("12"));
+																					"SCI-FI",	new	BigDecimal("12.00"));
 					Sessao	sessao	=	new	Sessao(LocalTime.parse("10:00:00"),	filme,	sala);
 					Ingresso	ingresso	=	new	Ingresso(sessao,	new	SemDesconto());
 					BigDecimal	precoEsperado	=	new	BigDecimal("32.50");
